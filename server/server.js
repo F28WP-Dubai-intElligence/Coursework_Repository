eeded9629894413df334d290f63a3ecc687a1cb4
shreyaa1910 <71345917+shreyaa1910@users.noreply.github.com>
2020-11-17 23:34:44 +0400

    server.js pt2

--- server/server.js
+++ server/server.js
@@ -20,3 +20,22 @@ if (process.env.NODE_ENV === 'development') {
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
 console.log(`Server listening on port ${port}`);
+const socketio = require('socket.io');
+const Constants = require('../shared/constants');
+
+// Setup Express
+// ...
+const server = app.listen(port);
+console.log(`Server listening on port ${port}`);
+
+// Setup socket.io
+const io = socketio(server);
+
+// Listen for socket.io connections
+io.on('connection', socket => {
+  console.log('Player connected!', socket.id);
+
+  socket.on(Constants.MSG_TYPES.JOIN_GAME, joinGame);
+  socket.on(Constants.MSG_TYPES.INPUT, handleInput);
+  socket.on('disconnect', onDisconnect);
+});
