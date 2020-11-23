f85ec4b3d7337dead2040e992f8d8faea106ba24
kevinmathewv <71334299+kevinmathewv@users.noreply.github.com>
2020-11-23 18:34:30 +0400

    server side error fixed

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-// Set static folder
-app.use(express.static(path.join(__dirname, "public")))
+//use morgan middleware
+const morgan = require("morgan");
+app.use(morgan('dev'));
 
+app.use(express.static('/'));
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+// // Set static folder
+// app.use(express.static(path.join(__dirname, "/public/src/js/game.js")))
+
+app.get('/', (req, res) => {
+    res.sendFile('game.html', { root: __dirname });
+});
 
 // Start server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
