c05935c851533330975375af330949b2e0ddafe5
rgs2000 <71346259+rgs2000@users.noreply.github.com>
2020-11-24 21:14:48 +0400

    Update leaderboard.js

--- public/src/leaderboard.js
+++ public/src/leaderboard.js
@@ -2,9 +2,7 @@ function leaderboardscores() {
     $(document).ready(function() {
                 $("#leaderboard").click(function(e) {
                     var username = $("#username").val();
-                    // var pass = $("#psw").val();
-                    // var email = $("#email").val();
-                    var score = $("#score")
+                    var score = $("#score").val();
                     var user = { username: username, score: score }; //username is attribute
                     // console.log(name + "'" + pass);
                     $.post('/api/leaderboard', user, function(result) { //IF RESULT IS NULL NO GAME , IF RESULT IS USER THEN GET GAME;HTML
