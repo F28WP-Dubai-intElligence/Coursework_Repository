a7af680bc388b05983db4172991817179445ebab
kevinmathewv <71334299+kevinmathewv@users.noreply.github.com>
2020-11-25 21:58:36 +0400

    leaderboard completely functional

--- public/src/leaderboard.js
+++ public/src/leaderboard.js
@@ -8,20 +8,24 @@ function init() {
         var score = getCookie("score");
         var userScore = { username: username, score: score }; //username is attribute
         $.post('/api/leaderboardUpdate', userScore, function(resultUpd) {})
-        $.post('/api/leaderboard', function(result) { //IF RESULT IS NULL NO GAME , IF RESULT IS USER THEN GET GAME;HTML
-                console.log(result[0]);
-                var i = 0;
-                while (i < result.length) {
-                    $("#" + i + "user").html(result[i].username);
-                    $("#" + i + "score").html(result[i].score);
-                    i++;
-                }
-                console.log("test" + result.length);
-            })
-            .fail(function() {
-                window.alert("test1");
-                console.log("error loading user");
-            });
+        setTimeout(function() {
+            $.post('/api/leaderboard', function(result) { //IF RESULT IS NULL NO GAME , IF RESULT IS USER THEN GET GAME;HTML
+                    console.log(result[0]);
+                    var i = 0;
+                    while (i < result.length) {
+                        $("#" + i + "user").html(result[i].username);
+                        $("#" + i + "score").html(result[i].score);
+                        i++;
+                    }
+                    console.log("test" + result.length);
+                })
+                .fail(function() {
+                    window.alert("test1");
+                    console.log("error loading user");
+                });
+
+        }, 2000)
+
 
     })
 };
