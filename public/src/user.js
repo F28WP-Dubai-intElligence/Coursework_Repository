fd0567bac3c56e03c02ce53906e2b4b114738764
kevinmathewv <71334299+kevinmathewv@users.noreply.github.com>
2020-11-25 18:48:28 +0400

    undo

--- public/src/user.js
+++ public/src/user.js
@@ -12,7 +12,7 @@ function init() {
 
                     } else if (result.outcome == 2) {
                         sessionStorage.setItem('curUser', username);
-                        window.location.replace('public/src/game.html');
+                        window.location.replace('game.html');
                     }
                 })
                 .fail(function() {
