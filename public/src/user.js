3291f21a097358387f78d0a8b46c21fd27410e17
sk2015 <71325510+sohamkakraa@users.noreply.github.com>
2020-11-25 22:17:48 +0400

    validating email and password

--- public/src/user.js
+++ public/src/user.js
@@ -4,7 +4,8 @@ function init() {
             var username = $("#username").val();
             var pass = $("#psw").val();
             var email = $("#email").val();
-            var user = { username: username, password: pass, email: email }; //username is attribute
+            if( validatePassword(pass) === true && validateEmail(email) === true){
+            var user = { username: username, password: pass, email: email}//username is attribute
             $.post('/api/register', user, function(result) {
                     console.log("result: " + result.outcome);
                     if (result.outcome == 1 || result.outcome == 0) {
@@ -19,13 +20,20 @@ function init() {
                     window.alert(result);
                     console.log("error loading user");
                 });
-
+        } else if (validatePassword(pass) === false && validateEmail(email) === true){
+            window.alert("Please enter valid password");
+        } else if (validatePassword(pass) === true && validateEmail(email) === false){
+            window.alert("Please enter valid email");
+        } else {
+            window.alert("Please enter valid email and password");
+        }
         });
         $("#login").click(function(e) {
             var username = $("#username").val();
             var pass = $("#psw").val();
             var email = $("#email").val();
-            var user = { username: username, password: pass, email: email }; //username is attribute
+            if( validatePassword(pass) === true && validateEmail(email) === true){
+            var user = { username: username, password: pass, email: email}; //username is attribute
             $.post('/api/login', user, function(result) { //IF RESULT IS NULL NO GAME , IF RESULT IS USER THEN GET GAME;HTML
                     console.log("result: " + result.outcome);
                     if (result.outcome == 2) {
@@ -38,6 +46,23 @@ function init() {
                 .fail(function() {
                     console.log("error loading user");
                 });
+            }else if (validatePassword(pass) === false && validateEmail(email) === true){
+                window.alert("Please enter valid password");
+            } else if (validatePassword(pass) === true && validateEmail(email) === false){
+                window.alert("Please enter valid email");
+            } else {
+                window.alert("Please enter valid email and password");
+            }
         })
     });
+}
+
+function validateEmail(elementValue){      
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue); 
+} 
+
+function validatePassword(elementValue) {
+    var passwordPattern = /(?=.*\d).{8,}/;
+    return passwordPattern.test(elementValue)
 }
