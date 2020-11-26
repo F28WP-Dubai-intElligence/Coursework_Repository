1fdc5bafac46029cbc5732dd5f299e264d155d9c
kevinmathewv <71334299+kevinmathewv@users.noreply.github.com>
2020-11-26 21:43:01 +0400

    minor change

--- public/src/leaderboard.js
+++ public/src/leaderboard.js
@@ -6,29 +6,29 @@
 // the function that runs everytime the leaderboard.html or gameover.html files are open since both need the leaderboard to be displayed
 function init() {
     $(document).ready(function() {
-        var username = getCookie("username");
-        var score = getCookie("score");
-        var userScore = { username: username, score: score };
-        $.post('/api/leaderboardUpdate', userScore, function(resultUpd) {})
-        setTimeout(function() {
-            $.post('/api/leaderboard', function(result) {
-                    // console.log(result[0]);
-                    var i = 0;
-                    while (i < result.length) {
-                        $("#" + i + "user").html(result[i].username);
-                        $("#" + i + "score").html(result[i].score);
-                        i++;
-                    }
-                    console.log("test" + result.length);
-                })
-                .fail(function() {
-                    window.alert("test1");
-                    console.log("error loading user");
-                });
+        if (getCookie("username") != "" && getCookie("score") != "") {
+            var username = getCookie("username");
+            var score = getCookie("score");
+            var userScore = { username: username, score: score };
+            $.post('/api/leaderboardUpdate', userScore, function(resultUpd) {})
+            setTimeout(function() {
+                $.post('/api/leaderboard', function(result) {
+                        // console.log(result[0]);
+                        var i = 0;
+                        while (i < result.length) {
+                            $("#" + i + "user").html(result[i].username);
+                            $("#" + i + "score").html(result[i].score);
+                            i++;
+                        }
+                        console.log("test" + result.length);
+                    })
+                    .fail(function() {
+                        window.alert("test1");
+                        console.log("error loading user");
+                    });
 
-        }, 2000)
-
-
+            }, 2000)
+        }
     })
     setCookie("score", "", 30);
     setCookie("username", "", 30);
